31ce9e0ea37dd43514b9ea91f83d663c9f5df485
정진수 <zxcv423@naver.com>
2023-02-01 23:10:12 +0900

    second commit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -14,6 +14,7 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 let roomName;
+let myPeerConnection;
 
 async function getCameras(){
     try{
@@ -52,7 +53,6 @@ async function getMedia(deviceId){
         if(!deviceId){
             await getCameras();
         }
-       
     }catch(e){
         console.log(e);
     }
@@ -82,6 +82,11 @@ function handleCameraClick(){
 
 async function handleCameraChange(){
     await getMedia(cameraSelect.value);
+    if(myPeerConnection){
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection.getSenders().find((sender) => sender.track.kind === "video");
+        videoSender.replaceTrack(videoTrack);
+    }
 }
 
 muteBtn.addEventListener("click", handleMuteClick);
@@ -92,17 +97,19 @@ cameraSelect.addEventListener("input", handleCameraChange);
 const welcome = document.getElementById("welcome");
 welcomeForm = welcome.querySelector("form");
 
-function startMedia(){
+async function initCall(){
     welcome.hidden = true;
     call.hidden = false;
-    getMedia();
+    await getMedia();
+    makeConnection();
 }
 
 
-function handleWelcomeSubmit(event){
+async function handleWelcomeSubmit(event) {
     event.preventDefault();
     const input = welcomeForm.querySelector("input");
-    socket.emit("join_room", input.value, startMedia);
+    await initCall();
+    socket.emit("join_room", input.value);
     roomName = input.value;
     input.value = "";
 }
@@ -111,6 +118,52 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
 // Socket Code
 
-socket.on("welcome", () => {
-    console.log("someone joined");
+socket.on("welcome", async () => {
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    socket.emit("offer", offer, roomName)
 })
+
+socket.on("offer", async (offer) => {
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer", answer, roomName);
+})
+
+socket.on("answer", (answer) => {
+    myPeerConnection.setRemoteDescription(answer);
+})
+
+socket.on("ice", (ice) => {
+    myPeerConnection.addIceCandidate(ice);
+})
+
+// RTC Code
+function makeConnection(){
+    myPeerConnection = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls: [
+                    "stun:stun.l.goole.com:19302",
+                    "stun:stun1.l.goole.com:19302",
+                    "stun:stun2.l.goole.com:19302",
+                    "stun:stun3.l.goole.com:19302",
+                    "stun:stun4.l.goole.com:19302",
+                ],
+            },
+        ],
+    });
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));
+    myPeerConnection.addEventListener("addstream", handleAddStream);
+}
+
+function handleIce(data){
+    socket.emit("ice", data.candidate, roomName);
+}
+
+function handleAddStream(data){
+    const peerFace = document.getElementById("peerFace");
+    peerFace.srcObject = data.stream;
+}
